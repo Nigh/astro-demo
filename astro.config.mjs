874bd4f98e69732836397d50e9c948aa16a3a207
xianii <xianyi.xia@outlook.com>
2025-04-13 20:59:23 +0800

    test workflow env

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import svelte from '@astrojs/svelte';
+import tailwindcss from '@tailwindcss/vite';
 import dotenv from 'dotenv';
 dotenv.config();
 
-import svelte from '@astrojs/svelte';
-
-import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte()],
